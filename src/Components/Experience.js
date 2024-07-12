f3d855a2a21aa01f24d38651e12db29d33b60514
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2024-07-12 09:19:39 -0400

    Added experience component

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,111 +1,77 @@
-import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 function Experience() {
-  // turn of sticky navbar
-  
   return (
-      <div style={{
-        backgroundImage: 'linear-gradient(to right, #ADD8E6, #87CEEB)', // Replace these color values with your desired gradient
-      }}>
-      <VerticalTimeline>
+    <div>
+      <h1 className="text-4xl font-semibold text-gradient text-center pt-24">
+        Experience
+      </h1>
+      <VerticalTimeline lineColor="#000">
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(255, 255, 255)', color: '#000000' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(0, 0, 0)' }}
+          contentStyle={{ background: '#333', color: '#fff', border: '3px solid #fdba74' }}
+          contentArrowStyle={{ borderRight: '7px solid  #333' }}
+          date="May 2024 - Aug 2024"
+          iconStyle={{ background: '#fed7aa', color: '#fff', boxShadow: '0 0 0 4px black' }}
+         
+        >
+          <h3 className="font-raleway text-2xl font-bold">TTC (Toronto Transit Commission)</h3>
+          <h4 className="vertical-timeline-element-subtitle italic">Software Developer</h4>
+          <p>Created internal tools using C#, .NET, Javascript, and SQL.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#333', color: '#fff', border: '3px solid #fdba74' }}
+          contentArrowStyle={{ borderRight: '7px solid  #333' }}
           date="Jun 2022  - Aug 2022"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        
+          iconStyle={{ background: '#fed7aa', color: '#fff', boxShadow: '0 0 0 4px black' }}
+         
         >
-          <h3 className="vertical-timeline-element-title">CAA</h3>
+          <h3 className="font-raleway text-2xl font-bold">CAA</h3>
           <h4 className="vertical-timeline-element-subtitle">Membership Support Intern</h4>
-          <p>
-            Directly responsible for customer calls and processed large-scale transactions ranging from $100 to
-            $1000
-          </p>
+          <p>Directly responsible for customer calls and processed large-scale transactions ranging from $100 to $1000</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="Mar 2022 - Jun 2022"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          
-        >
-          <h3 className="vertical-timeline-element-title">Dairy Queen</h3>
-          <h4 className="vertical-timeline-element-subtitle">Crew Member</h4>
-          <p>
-            Managed and processed customer orders.
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#333', color: '#fff', border: '3px solid #fdba74' }}
+          contentArrowStyle={{ borderRight: '7px solid  #333' }}
           date="Nov 2021 - Dec 2022"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{ background: '#fed7aa', color: '#fff', boxShadow: '0 0 0 4px black' }}
           
         >
-          <h3 className="vertical-timeline-element-title">Kurius</h3>
+          <h3 className="font-raleway text-2xl font-bold">Kurius</h3>
           <h4 className="vertical-timeline-element-subtitle">Partnerships Coordinator</h4>
-          <p>
-            Raised sponsors and over $6000 in prize money for KuriusHacks CE.
-          </p>
+          <p>Raised sponsors and over $6000 in prize money for KuriusHacks CE.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
+          contentStyle={{ background: '#333', color: '#fff', border: '3px solid #fdba74' }}
+          contentArrowStyle={{ borderRight: '7px solid  #333' }}
           date="Jun 2021 - Aug 2021"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{ background: '#fed7aa', color: '#fff', boxShadow: '0 0 0 4px black' }}
           
         >
-          <h3 className="vertical-timeline-element-title">e2youngengineers</h3>
+          <h3 className="font-raleway text-2xl font-bold">e2youngengineers</h3>
           <h4 className="vertical-timeline-element-subtitle">STEM Instructor</h4>
-          <p>
-          Prepared and served a variety of lessons with precision and creativity to over 25 students, consistently
-meeting and exceeding quality standards.
-          </p>
+          <p>Prepared and served a variety of lessons with precision and creativity to over 25 students, consistently meeting and exceeding quality standards.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
+          contentStyle={{ background: '#333', color: '#fff', border: '3px solid #fdba74' }}
+          contentArrowStyle={{ borderRight: '7px solid  #333' }}
           date="2019 - 2023"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          iconStyle={{ background: '#fed7aa', color: '#fff', boxShadow: '0 0 0 4px black' }}
           
         >
-          <h3 className="vertical-timeline-element-title">Robotics Programming Team Lead</h3>
+          <h3 className="font-raleway text-2xl font-bold">Robotics Programming Team Lead</h3>
           <h4 className="vertical-timeline-element-subtitle">Bishop Reding Catholic Secondary School</h4>
-          <p>
-            Led school robotics team in programming. Developed image recogntion models using openCV
-            and taught C to students.
-          </p>
+          <p>Led school robotics team in programming. Developed image recognition models using openCV and taught C to students.</p>
         </VerticalTimelineElement>
-        {/* <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        
-        >
-          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        
-        >
-          <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-          <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-          <p>
-            Creative Direction, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-        
-        /> */}
       </VerticalTimeline>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,12 +3,14 @@ import React from 'react'
 import  About  from './About'
 import  Projects from './Projects'
 import Skills from './Skills'
+import Experience from './Experience'
 function Home() {
   return (
       <>
       <About></About>
       <Projects></Projects>
       <Skills></Skills>
+      <Experience></Experience>
       </>
   )
 }
